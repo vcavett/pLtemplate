358b0682f0d116826f59df5895ea7b89995515fd
Val Cavett <vcavett@gmail.com>
2021-05-05 18:41:31 -0700

    fixed the formatting in roxygen @example to get rid of the errors!

--- R/theme_pL.R
+++ R/theme_pL.R
@@ -1,67 +1,66 @@
-usethis::use_package("ggplot2")
-usethis::use_package("magrittr")
-
-#' Paegel lab theme for ggplot graphics.
+#' Paegel lab theme for ggplot2 graphics.
 #'
-#' Formats ggplot graphics in a consistent style for use in notebooks and publications.
+#' Formats ggplot2 graphics in a consistent style for use in notebooks and publications.
 #' @return formatted plot
 #' @export
+#' @import magrittr
+#' @import ggplot2
 #'
-#' @examples
-#' data <- tibble(min = 0:10,  Y = sin(min))
-#' data %>% ggplot() + aes(min, Y) + geom_line() + theme_pL()
+#' @example
+#' data <- tibble(x = 0:100, y = sin(x))
+#' @example
+#' ggplot() + aes(x, y) + geom_line() + theme_pL()
 
 theme_pL <- function(){
 
     font <- "Helvetica"   #assign font family up front
 
-    theme_bw() %+replace%    #replace elements we want to change
-
-    theme(
+    ggplot2::theme_bw() #%+replace%    #replace elements we want to change
+    ggplot2::theme_replace(
 
       #grid elements
-      panel.grid.major = element_blank(),    #strip major gridlines
-      panel.grid.minor = element_blank(),    #strip minor gridlines
-      axis.ticks = element_blank(),          #strip axis ticks
+      panel.grid.major = ggplot2::element_blank(),    #strip major gridlines
+      panel.grid.minor = ggplot2::element_blank(),    #strip minor gridlines
+      axis.ticks = ggplot2::element_blank(),          #strip axis ticks
 
       #text elements
-      plot.title = element_text(             #title
+      plot.title = ggplot2::element_text(             #title
                    family = font,            #set font family
                    size = 18,                #set font size
                    face = 'bold',            #bold typeface
                    hjust = 0.5,                #left align
                    vjust = 2,
-                   margin = margin(t = 5)),               #raise slightly
+                   margin = ggplot2::margin(t = 5)),               #raise slightly
 
-      plot.subtitle = element_text(          #subtitle
+      plot.subtitle = ggplot2::element_text(          #subtitle
                    family = font,            #font family
                    size = 12,
-                   margin = margin(b = 5)),               #font size
+                   margin = ggplot2::margin(b = 5)),               #font size
 
-      plot.caption = element_text(           #caption
+      plot.caption = ggplot2::element_text(           #caption
                    family = font,            #font family
                    size = 9,                 #font size
                    hjust = 1),               #right align
 
-      axis.title = element_text(             #axis titles
+      axis.title = ggplot2::element_text(             #axis titles
                    family = font,            #font family
                    size = 12,
                    face = 'bold'),  #font size
 
-      axis.title.y = element_text(margin = margin(10,10,10,10),
+      axis.title.y = ggplot2::element_text(margin = ggplot2::margin(10,10,10,10),
       														angle = 90),
 
-      axis.text = element_text(              #axis text
+      axis.text = ggplot2::element_text(              #axis text
                    family = font,            #axis famuly
                    size = 9,
-                   margin = margin(3,3,3,3)),                #font size
+                   margin = ggplot2::margin(3,3,3,3)),                #font size
 
-      axis.text.x = element_text(            #margin for axis text
-                    margin=margin(3,3,3,3)),
+      axis.text.x = ggplot2::element_text(            #margin for axis text
+                    margin = ggplot2::margin(3,3,3,3)),
 
-      legend.title = element_blank(),
+      legend.title = ggplot2::element_blank(),
 
-      panel.background = element_rect(size = 2)
+      panel.background = ggplot2::element_rect(size = 2)
       #since the legend often requires manual tweaking
       #based on plot content, don't define it here
     )
